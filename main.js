846526718bbfe8a30a4654944e24f23383169578
Enoch <65424112+enochsori@users.noreply.github.com>
2022-02-07 12:23:32 -0500

    JS : Arrow up

--- main.js
+++ main.js
@@ -50,6 +50,21 @@ document.addEventListener('scroll', () => {
   home.style.opacity = homeOpacity;
 });
 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+    console.log(window.scrollY);
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
+arrowUp.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
