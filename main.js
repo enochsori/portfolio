d54a4f90467b83c098b1b29c5eb1c4b348205201
enochsori <enoch.ha915@gmail.com>
2022-08-10 16:20:22 -0400

    update contents

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ workBtnContainer.addEventListener('click', (e) => {
 
 // 1. 모든 섹션 요소들을 가지고 온다.
 // 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
-// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.`
 
 // [Implement] 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다.
 const sectionIds = [
